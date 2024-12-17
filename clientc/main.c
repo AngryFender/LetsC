460f03d1d1a16c41fc6f139deeb76f0181842b0a
R <Teabiscuitperson@gmail.com>
2024-12-17 23:41:34 +0000

    Fixed indentation since the last commit was done on a phone

--- clientc/main.c
+++ clientc/main.c
@@ -2,23 +2,23 @@
 #include "../library.h"
 
 void printDebug(const char* message){
-				printf("%s\n",message);
+	printf("%s\n",message);
 }
 
 void printInfo(const char* message){
-	   printf("%s\n", message);
+	printf("%s\n", message);
 }
 
 void printWarning(const char* message){
-	   printf("%s\n", message);
+	printf("%s\n", message);
 }
 
 void printError(const char* message){
-	   printf("%s\n", message);
+	printf("%s\n", message);
 }
 
 void printFatal(const char* message){
-	   printf("%s\n", message);
+	printf("%s\n", message);
 }
 
 int main(){
