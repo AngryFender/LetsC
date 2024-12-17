f47dd2ba1129779390828482f786f2dbd1d74df0
R <Teabiscuitperson@gmail.com>
2024-12-17 23:40:21 +0000

    Added functions to print all levels of logs

--- clientc/main.c
+++ clientc/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include "../library.h"
+
+void printDebug(const char* message){
+				printf("%s\n",message);
+}
+
+void printInfo(const char* message){
+	   printf("%s\n", message);
+}
+
+void printWarning(const char* message){
+	   printf("%s\n", message);
+}
+
+void printError(const char* message){
+	   printf("%s\n", message);
+}
+
+void printFatal(const char* message){
+	   printf("%s\n", message);
+}
+
 int main(){
     printf("Starting ClientC\n");
     hello();
